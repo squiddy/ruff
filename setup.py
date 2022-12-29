b521cede7b71cf255d4a59a45a0bb3cc87b0ed93
Reiner Gerecke <me@reinergerecke.de>
2022-12-29 11:36:39 +0100

    tmp

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import platform
+
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension, RustBin, Strip
+
+if platform.machine() in ["aarch64", "armv7", "s390x", "ppc64le", "ppc64"]:
+    args = ["--no-default-features"]
+else:
+    args = []
 
 # We can't specify this in pyproject.toml just yet: https://github.com/PyO3/setuptools-rust/issues/208
 setup(
     name="ruff",
     rust_extensions=[
-        RustBin("ruff", strip=Strip.Debug),
+        RustBin("ruff", strip=Strip.Debug, args=args),
     ],
     packages=["ruff"],
     # rust extensions are not zip safe, just like C-extensions.
